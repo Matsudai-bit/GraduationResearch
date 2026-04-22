639fe064fe35b193600e24ff6787ac9903d57107
松下大暉 <85777234+Matsudai-bit@users.noreply.github.com>
2026-04-22 10:04:38 +0900

    スローモーションの仮クラスの作成

--- Assets/Project/Scripts/CharacterController.cs
+++ Assets/Project/Scripts/CharacterController.cs
@@ -3,20 +3,31 @@ using UnityEngine.Playables;
 
 public class CharacterController : MonoBehaviour
 {
-    private Animator m_animator;
-    private ParticleSystem m_particle;
-    private PlayableDirector m_director;
+    private Animator            m_animator;
+    private ParticleSystem      m_particle;
+    private PlayableDirector    m_director;
+    [SerializeField]
+    private TrailRenderer       m_trailRenderer;
+
+    CharacterSpeed              m_characterSpeed = new();
 
     private void Awake()
     {
-        m_animator = GetComponent<Animator>();
-        m_particle = GetComponent<ParticleSystem>();
-        m_director = GetComponent<PlayableDirector>();
+        m_animator      = (!m_animator)     ? GetComponent<Animator>()          : m_animator;
+        m_particle      = (!m_particle)     ? GetComponent<ParticleSystem>()    : m_particle;
+        m_director      = (!m_director)     ? GetComponent<PlayableDirector>()  : m_director;
+        m_trailRenderer = (!m_trailRenderer)? GetComponent<TrailRenderer>()     : m_trailRenderer;
     }
 
     private void Start()
     {
-        UpdateSpeed(0.1f);
+        m_characterSpeed.onChangeBaseSpeed = UpdateSpeed;
+
+   //     UpdateSpeed(0.1f);
+
+        m_characterSpeed.Initialize();
+
+        m_characterSpeed.onChangeBaseSpeed?.Invoke(0.5f);
     }
 
     private void UpdateSpeed(float speed)
@@ -33,6 +44,11 @@ public class CharacterController : MonoBehaviour
             var main = m_particle.main;
             main.simulationSpeed = speed;
 
+        }
+
+        if (m_trailRenderer)
+        {
+            m_trailRenderer.time /= speed;
         }
 
         // Timelineのスピード更新
--- Assets/Project/Scripts/Speed/CharacterSpeed.cs
+++ Assets/Project/Scripts/Speed/CharacterSpeed.cs
@@ -6,18 +6,20 @@ public class CharacterSpeed
     private readonly GameSpeed m_baseSpeed = new();  // 基準の速さ
     public float CurrentSpeed => m_baseSpeed.CurrentSpeed ;
 
-    public Action<float> m_onChangeBaseSpeed;   // ベース速度が変更された時に呼ばれる
+    public Action<float> onChangeBaseSpeed;   // ベース速度が変更された時に呼ばれる
 
     /// <summary>
     /// 初期化処理
     /// </summary>
     public void Initialize()
     {
+
+
         // ベース速度の値が変更された時の処理を登録
         m_baseSpeed.OnValueChange = baseSpeed =>
         {
             // 
-            m_onChangeBaseSpeed(baseSpeed);
+            onChangeBaseSpeed?.Invoke(baseSpeed);
         };
     }
 
